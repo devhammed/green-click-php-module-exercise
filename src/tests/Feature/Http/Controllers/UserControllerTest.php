eb500d6ddd5fad2f9fcedc6d85f10b3025b5359e
Hammed Oyedele <itz.harmid@gmail.com>
2024-12-21 17:45:35 +0100

    fix: update phpunit config

--- src/tests/Feature/Http/Controllers/UserControllerTest.php
+++ src/tests/Feature/Http/Controllers/UserControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,10 +12,17 @@ class UserControllerTest extends TestCase
 
     public function test_it_can_visit_users()
     {
+	$users = User::factory(10)->create();
+
         $response = $this->get('/users');
 
         $response->assertOk();
 
-        $response->assertSee('Users');
+	$response->assertSee('Users');
+
+	foreach ($users as $user) {
+	    $response->assertSee($user->name);
+	    $response->assertSee($user->email);
+	}
     }
 }
