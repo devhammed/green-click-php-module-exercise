9f78243c79035f279b91bb1348619277723abe09
Hammed Oyedele <itz.harmid@gmail.com>
2024-12-21 18:48:15 +0100

    fix: update the user module

--- src/modules/User/Http/Controllers/UserController.php
+++ src/modules/User/Http/Controllers/UserController.php
@@ -2,12 +2,33 @@
 
 namespace Modules\User\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Modules\User\Models\User;
+use Modules\User\Http\Resources\UserResource;
 
-class UserController extends Controller
+class UserController
 {
-    public function index()
+    public function index(): mixed
     {
-        return view('users');
+        return view('users', [
+            'users' => User::all(),
+        ]);
+    }
+
+    public function enable(User $user): UserResource
+    {
+        $user->update([
+            'enabled' => true,
+        ]);
+
+        return new UserResource($user);
+    }
+
+    public function disable(User $user): UserResouce
+    {
+        $user->update([
+            'enabled' => false,
+        ]);
+
+        return new UserResource($user);
     }
 }
